8d4d3e63fd718a900ef54c41f6c144507b91f598
ntluan <luanga316@gmail.com>
2023-02-21 09:38:59 +0700

    create program print 1 to 10 in java

---
+++ for1to10.java
@@ -0,0 +1,14 @@
+public class for1to10 
+{
+	public static void main(String[] args)
+	{
+		//print the result
+		System.out.println("Output is : ");
+
+		//loop  to print 1 to 10.
+		for(int i = 1; i <= 10; i++)
+		{
+			System.out.println(i);
+		}
+	}
+}
